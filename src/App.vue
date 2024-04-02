e41b7e4180f87b683f6ed45b3c74f91ee5a64ac7
LucaB98 <lucabellan98@gmail.com>
2024-04-02 19:07:36 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+export default {
+  name: 'Boolfolio',
+  components: {
+    AppHeader
+  }
+};
+</script>
+
+<template>
+
+  <AppHeader />
+  <main class="container my-4">
+
+
+  </main>
+
+</template>
+
+<style scoped></style>
